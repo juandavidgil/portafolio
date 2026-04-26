4870693971bf5ebf22da2e87b3a964c2ba9aba8d
Nikolas2407 <nicolasmejianarvaez@gmail.com>
2026-04-25 23:17:20 -0500

    barra

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,28 +1,19 @@
-import { useState } from "react";
-import { Menu, X } from "lucide-react";
+import '../App.css'
 
-import "../App.css";
+function Navbar() {
+    return (
+        <>
+        <nav className="navbar">
+            <ul className="navbar-list">
+                <li className="navbar-item"><a href="#home">Sobre Mi</a></li>
+                <li className="navbar-item"><a href="#about">Proyectos</a></li>
+                <li className="navbar-item"><a href="#projects">Inicio</a></li>
+                <li className="navbar-item"><a href="#contact">Habilidades</a></li>
+                <li className="navbar-item"><a href="#contact">Contacto</a></li>
+            </ul>
+        </nav>
 
-export default function Navbar() {
-  const [open, setOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setOpen(!open);
-  };
-
-  return (
-    <header className="navbar">
-      <nav className={`nav-menu ${open ? "active" : ""}`}>
-        <a href="#about" onClick={() => setOpen(false)}>Sobre mí</a>
-        <a href="#projects" onClick={() => setOpen(false)}>Proyectos</a>
-        <a href="#home" onClick={() => setOpen(false)}>Inicio</a>
-        <a href="#projects" onClick={() => setOpen(false)}>Habilidades</a>
-        <a href="#contact" onClick={() => setOpen(false)}>Contacto</a>
-      </nav>
-
-      <button className="menu-btn" onClick={toggleMenu}>
-        {open ? <X size={28} /> : <Menu size={28} />}
-      </button>
-    </header>
-  );
+        </>
+    );
 }   
+export default Navbar;  
